e9e9ab4e03551ceb24e54d60f5cdd89e0929c01e
XXTXTOP <xxtxtop@gmail.com>
2023-07-11 15:29:29 +0800

    add

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+# 追加当前时间到文件
+echo "$current_time" >> time.txt
---
+++ time.py
@@ -0,0 +1,10 @@
+import subprocess
+import time
+
+while True:
+
+    subprocess.call(['/home/Deepin/Desktop/update-kernel/script.sh'])
+
+
+    time.sleep(30)
+
